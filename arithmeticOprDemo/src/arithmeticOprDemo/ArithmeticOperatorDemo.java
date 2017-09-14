2529662911c9b0b1be035661a37bc3725279b13b
renuka <renutarse1@gmail.com>
2017-09-14 15:30:54 +0530

    arithmeticdemo commit

---
+++ arithmeticOprDemo/src/arithmeticOprDemo/ArithmeticOperatorDemo.java
@@ -0,0 +1,20 @@
+package arithmeticOprDemo;
+
+public class ArithmeticOperatorDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num1 = 100;
+		      int num2 = 20;
+			
+			      System.out.println("num1 + num2: " + (num1 + num2) );
+			      System.out.println("num1 - num2: " + (num1 - num2) );
+			      System.out.println("num1 * num2: " + (num1 * num2) ); 
+			      System.out.println("num1 / num2: " + (num1 / num2) );
+			      System.out.println("num1 % num2: " + (num1 % num2) );
+			   }
+			}
+
+	
+
+
